53704fe432b5ee6bd285770bc5cb5944058ef15c
byronklep <byronklep@gmail.com>
2021-06-10 17:59:17 -0500

    Footer styling.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,119 +3,6 @@ import React from 'react'
 const Footer = () => {
   return (
     <div>
-      {/* <footer className="footer bg-light text-center text-lg-start">
-        <div className="container p-4">
-          <div className="row">
-            <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
-              <h5 className="text-uppercase">Links</h5>
-
-              <ul className="list-unstyled mb-0">
-                <li>
-                  <a href="#!" className="text-dark">
-                    Link 1
-                  </a>
-                </li>
-                <li>
-                  <a href="#!" className="text-dark">
-                    Link 2
-                  </a>
-                </li>
-                <li>
-                  <a href="#!" className="text-dark">
-                    Link 3
-                  </a>
-                </li>
-                <li>
-                  <a href="#!" className="text-dark">
-                    Link 4
-                  </a>
-                </li>
-              </ul>
-            </div>
-
-            <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
-              <h5 className="text-uppercase mb-0">Links</h5>
-
-              <ul className="list-unstyled">
-                <li>
-                  <a href="#!" className="text-dark">
-                    Link 1
-                  </a>
-                </li>
-                <li>
-                  <a href="#!" className="text-dark">
-                    Link 2
-                  </a>
-                </li>
-                <li>
-                  <a href="#!" className="text-dark">
-                    Link 3
-                  </a>
-                </li>
-                <li>
-                  <a href="#!" className="text-dark">
-                    Link 4
-                  </a>
-                </li>
-              </ul>
-            </div>
-
-            <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
-              <h5 className="text-uppercase">Links</h5>
-
-              <ul className="list-unstyled mb-0">
-                <li>
-                  <a href="#!" className="text-dark">
-                    Link 1
-                  </a>
-                </li>
-                <li>
-                  <a href="#!" className="text-dark">
-                    Link 2
-                  </a>
-                </li>
-                <li>
-                  <a href="#!" className="text-dark">
-                    Link 3
-                  </a>
-                </li>
-                <li>
-                  <a href="#!" className="text-dark">
-                    Link 4
-                  </a>
-                </li>
-              </ul>
-            </div>
-
-            <div className="col-lg-3 col-md-6 mb-4 mb-md-0">
-              <h5 className="text-uppercase mb-0">Links</h5>
-
-              <ul className="list-unstyled">
-                <li>
-                  <a href="#!" className="text-dark">
-                    Link 1
-                  </a>
-                </li>
-                <li>
-                  <a href="#!" className="text-dark">
-                    Link 2
-                  </a>
-                </li>
-                <li>
-                  <a href="#!" className="text-dark">
-                    Link 3
-                  </a>
-                </li>
-                <li>
-                  <a href="#!" className="text-dark">
-                    Link 4
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div> */}
-
       <div
         className="text-center p-3"
         style={{ backgrounColor: 'rgba(0, 0, 0, 0.2)' }}>
@@ -124,7 +11,6 @@ const Footer = () => {
           Crypto-Verse
         </a>
       </div>
-      {/* </footer> */}
     </div>
   )
 }
